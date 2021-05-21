035a03aee23fd9bb4a012ddf482a1d556a17e31c
TAPASYA1234 <tapasyaprajapati1234@gmail.com>
2021-05-21 17:39:06 +0530

    connected

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          check intall
+          check intall of git
         </a>
       </header>
     </div>
